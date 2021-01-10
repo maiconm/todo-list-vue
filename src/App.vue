b5e7bc8838dda13e91e048d838cea7f930f6890c
maiconm <maiconm23@gmail.com>
2021-01-10 18:47:57 -0300

    Adiciona componente ToDoList em branco

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <Header />
+    <ToDoList />
   </div>
 </template>
 
 <script>
 import Header from './components/Header'
+import ToDoList from './components/ToDoList'
 export default {
   name: 'App',
   components: {
+    ToDoList,
     Header
   }
 }
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,12 @@
+<template>
+  <p>ToDoList works!</p>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+  
+</style>
